55d65c752be11e18cea90eb08be06dde526da6fe
adelguit <adeelguitoun@gmail.com>
2023-02-24 17:55:48 +0100

    Add user model associations and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # validation
+  validates :f_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true
+  validates :password, presence: true
+
+  # Associations
+  has_many :rooms
+  has_many :reservations
 end
