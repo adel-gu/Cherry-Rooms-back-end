f06651eb45ee94f5881310b28388c673b8a9568b
Zuhaib Amjad <umar0423@gmail.com>
2023-02-25 16:23:35 +0500

    Add unit testing to the room-model

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,7 +1,7 @@
 class Room < ApplicationRecord
   belongs_to :user
 
-  validates :name, presence: true
+  validates :name, format: {with: /[a-zA-Z]/}, presence: true
   validates :beds, numericality: { only_integer: true, greater_than_or_equal_to: 0  } , presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 } , presence: true
   validates :description, length: { maximum: 250 }, presence: true
