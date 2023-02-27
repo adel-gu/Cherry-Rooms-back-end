86676f1e74c3867dd7e39674d41d1ab3574932f5
arabiu033 <arabiu033@gmail.com>
2023-02-27 13:59:04 +0100

    Run dm migrate to generate new schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_25_095005) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_27_112913) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.date "from_date"
+    t.date "to_date"
+    t.integer "number_of_persons"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.bigint "room_id", null: false
+    t.index ["room_id"], name: "index_reservations_on_room_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "name"
@@ -40,5 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_25_095005) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "rooms"
+  add_foreign_key "reservations", "users"
   add_foreign_key "rooms", "users"
 end
