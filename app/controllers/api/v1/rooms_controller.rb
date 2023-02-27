c3a69cc2533185ddb243f51f05ec9b54adf67fa9
Zuhaib Amjad <umar0423@gmail.com>
2023-02-27 15:33:36 +0500

    Fix linters errors

--- app/controllers/api/v1/rooms_controller.rb
+++ app/controllers/api/v1/rooms_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RoomsController < ApplicationController
-  before_action :set_room, only: %i[ show destroy ]
-  before_action :set_user, only: %i[ create destroy]
+  before_action :set_room, only: %i[show destroy]
+  before_action :set_user, only: %i[create destroy]
 
   # GET /rooms
   def index
@@ -15,17 +15,18 @@ class Api::V1::RoomsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_room
-      @room = Room.find(params[:id])
-    end
 
-    def set_user
-      @user = current_user
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_room
+    @room = Room.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def room_params
-      params.require(:room).permit(:name, :beds, :price, :description, :image, :city)
-    end
+  def set_user
+    @user = current_user
+  end
+
+  # Only allow a list of trusted parameters through.
+  def room_params
+    params.require(:room).permit(:name, :beds, :price, :description, :image, :city)
+  end
 end
