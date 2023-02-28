d8e6d53d580ee6b920322681053dd10c69c9c20a
adelguit <adeelguitoun@gmail.com>
2023-02-28 10:35:42 +0100

    Setup users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-
+  namespace :api do
+    namespace :v1 do
+      devise_for :users, path: '', path_names: {
+        sign_in: 'login',
+        sign_out: 'logout',
+        registration: 'signup'
+      },
+      controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
+    end
+  end
 end
