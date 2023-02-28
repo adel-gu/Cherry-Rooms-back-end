33e8623bf9af1737171479efe6c9a08cf1e90ec9
adelguit <adeelguitoun@gmail.com>
2023-02-28 11:52:08 +0100

    remove api/v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      devise_for :users, path: '', path_names: {
-        sign_in: 'login',
-        sign_out: 'logout',
-        registration: 'signup'
-      },
-      controllers: {
-        sessions: 'users/sessions',
-        registrations: 'users/registrations'
-      }
-    end
-  end
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
